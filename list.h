ede1c22ff21609905b2ad10391bb486c02a7233e
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2019-03-29 13:49:30 -0700

    Linked list implementation

--- list.h
+++ list.h
@@ -7,33 +7,54 @@
 
 template<typename T>
 struct Node {
-    Node() : prev(nullptr), next(nullptr), value(0) {}
+    Node() : prev(nullptr), value(0), next(nullptr) {}
 
-    Node(const T val) : prev(nullptr), next(nullptr), value(val) {}
+    Node(Node<T> *prev, const T val) : prev(prev), value(val) {}
 
-    Node(const Node<T> *node) : prev(node->prev), next(node->next), value(node->value) {}
+    Node(const T val, Node<T> *next) : value(val), next(next) {}
+
+    Node(Node<T> *prev, const T val, Node<T> *next) : prev(prev), value(val), next(next) {}
+
+    Node(const T val) : prev(nullptr), value(val), next(nullptr) {}
 
     Node<T> *prev;
     Node<T> *next;
     T value;
 };
 
-templete<typename T>
+template<typename T>
 class List {
 public:
     // Constructors
-    List() : head(nullptr), taile(nullptr), size(0) {}
-    List(const int size);
-    List(const List& rhs);
+    List() : head(nullptr), tail(nullptr), sz(0) {}
+
     ~List();
 
-    // Equality
+    // Element access:
+    T at(const unsigned int position);
+
+    T operator[](const unsigned int position);
+
+    // Modifiers:
+    void push_front(const T value);
+
+    void pop_front();
+
+    void push_back(const T value);
+
+    void pop_back();
+
+    void erase(const unsigned int position);
+
+    // Capacity:
+    unsigned int size() const { return size; }
+
 private:
     void subError();    // Handles memory subscripts out of range
 
     Node<T> *head;
     Node<T> *tail;
-    unsigned int size;
+    unsigned int sz;
 };
 
 #endif //DMON_LIST_H
