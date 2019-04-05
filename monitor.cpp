9811da5e1bbdcea486cfab655634fb636211bd72
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-04-04 23:42:55 -0700

    Event detection mvp

--- monitor.cpp
+++ monitor.cpp
@@ -7,41 +7,121 @@
 /*
  * Compares two trees and populates the events vector.
  */
-Index detect(TreeNode* prevTree, TreeNode* currTree, std::vector<Event>* events) {
-    int prevSize = prevTree->children.size();
-    int currSize = currTree->children.size();
-
-    for (int i = 0, j = 0; i < prevSize && j < currSize; i++, j++) {
-        Index index = detect(prevTree->children[i], currTree->children[j], events);
-        if (index == Index::PREV) { // Only increment prev index (i)
-            j--;      // Keep curr index (j) the same
-        } else if (index == Index::CURR) {  // Only increment curr index (j)
-            i--;      // Keep prev index (i) the same.
-        }
-        // Else increment both
-    }
-
-    // We need to be able to reach into the previous stack and manipulate i and j
-    // in order to keep the comparison in sync. This is why detect returns an Index.
-
+Index detect(TreeNode *prevTree, TreeNode *currTree, std::vector<Event> *events) {
     // This is the "Visit Function"
-
     if (prevTree->name == currTree->name) {         // Same Node
         // Check for modification
+        // We can skip over the entire subtree if the root node of
+        // that subtree does not reflect any modification.
         if (prevTree->timeStats.modified < currTree->timeStats.modified) {
-            std::cout << "MODIFIED: " << prevTree->fullPath << std::endl;
+            if (prevTree->type == PathType::DIRECTORY) {
+                // We need to be able to reach into the previous stack and manipulate i and j
+                // in order to keep the comparison in sync. This is why detect() returns an Index.
+                int prevSize = prevTree->children.size();
+                int currSize = currTree->children.size();
+
+                int i = 0;
+                int j = 0;
+                while (i < prevSize && j < currSize) {
+                    Index index = detect(prevTree->children[i], currTree->children[j], events);
+                    if (index == Index::PREV) { // Only increment prev index (i)
+                        i++;      // Keep curr index (j) the same
+                    } else if (index == Index::CURR) {  // Only increment curr index (j)
+                        j++;      // Keep prev index (i) the same.
+                    } else {
+                        // Else increment both
+                        i++;
+                        j++;
+                    }
+                }
+
+                // Deletion at the End
+                while (i < prevSize) {
+                    TreeNode *prevNode = prevTree->children[i];
+                    // TODO: Change this from pre to post order.
+                    // Get all the leaf nodes of prevNode
+                    std::vector<TreeNode *> stack;
+                    stack.push_back(prevNode);
+                    while (stack.size() > 0) {
+                        TreeNode *node = stack[stack.size() - 1];
+                        stack.pop_back();
+                        // If Leaf Node
+                        if (node->children.size() == 0) {
+                            std::cout << "DELETED: " << node->fullPath << std::endl;
+                        } else {
+                            for (int i = node->children.size() - 1; i >= 0; i--) {
+                                stack.push_back(node->children[i]);
+                            }
+                        }
+                    }
+                    i++;
+                }
+
+                // Creation at the End
+                while (j < currSize) {
+                    TreeNode *currNode = currTree->children[j];
+                    // TODO: Change this from pre to post order.
+                    // Get all the leaf nodes of currNode
+                    std::vector<TreeNode *> stack;
+                    stack.push_back(currNode);
+                    while (stack.size() > 0) {
+                        TreeNode *node = stack[stack.size() - 1];
+                        stack.pop_back();
+                        // If Leaf Node
+                        if (node->children.size() == 0) {
+                            std::cout << "CREATED: " << node->fullPath << std::endl;
+                        } else {
+                            for (int i = node->children.size() - 1; i >= 0; i--) {
+                                stack.push_back(node->children[i]);
+                            }
+                        }
+                    }
+                    j++;
+                }
+            } else {
+                std::cout << "MODIFIED: " << prevTree->fullPath << std::endl;
+            }
         }
+
         return Index::BOTH;
     } else if (prevTree->name < currTree->name) {   // prevTree node deleted
-        std::cout << "DELETED: " << prevTree->fullPath << std::endl;
+        // TODO: Change this from pre to post order
+        // Get all the leaf nodes of prevTree
+        std::vector<TreeNode *> stack;
+        stack.push_back(prevTree);
+        while (stack.size() > 0) {
+            TreeNode *node = stack[stack.size() - 1];
+            stack.pop_back();
+            // If Leaf Node
+            if (node->children.size() == 0) {
+                std::cout << "DELETED: " << node->fullPath << std::endl;
+            } else {
+                for (int i = node->children.size() - 1; i >= 0; i--) {
+                    stack.push_back(node->children[i]);
+                }
+            }
+        }
+
         return Index::PREV;
-        // TODO: Handle nesting if DIR
     } else {                                        // currTree node created
-        std::cout << "CREATED: " << currTree->fullPath << std::endl;
+        // TODO: Change this from pre to post order.
+        // Get all the leaf nodes of currTree
+        std::vector<TreeNode *> stack;
+        stack.push_back(currTree);
+        while (stack.size() > 0) {
+            TreeNode *node = stack[stack.size() - 1];
+            stack.pop_back();
+            // If Leaf Node
+            if (node->children.size() == 0) {
+                std::cout << "CREATED: " << node->fullPath << std::endl;
+            } else {
+                for (int i = node->children.size() - 1; i >= 0; i--) {
+                    stack.push_back(node->children[i]);
+                }
+            }
+        }
+
         return Index::CURR;
-        // TODO: Handle nesting if DIR
     }
-
-
 }
 
